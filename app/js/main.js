8e4b3b4b8571a12824b7f182eeebe76267abb303
chkalov12 <chkalov007@gmail.com>
2020-12-04 22:58:26 +0200

    add header

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,10 @@
-$(function(){
-	//  $('.header__btn').click(function(){
-	//  	$('html, body').animate({scrollTop: $(document).height() - $(window).height()}, 15000);
-	//  	return false;
-  //    });
+$(function() {
+    //  $('.header__btn').click(function(){
+    //  	$('html, body').animate({scrollTop: $(document).height() - $(window).height()}, 15000);
+    //  	return false;
+    //    });
 
 
-     $('.header__btn').click(function(e){
-      e.preventDefault();
-      $('body, html').animate({ scrollTop: $('.form').offset().top }, 10000);
-      });
 
-    
-    $('.burger-btn').on('click', function(){
-        $('.menu__list').slideToggle();
-      });
-
-      new WOW().init();
 
 });
